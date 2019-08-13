df36ad72d05d7accee2144b49a8247748e57a338
Daniel Vanegas <danieru1313@gmail.com>
2019-08-13 17:27:53 -0500

    Add firebase login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
 
-export default App;
+import withFirebaseAuth, { WrappedComponentProps } from 'react-with-firebase-auth';
+
+import firebaseConfig from './firebaseConfig';
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const App = ({
+  /** These props are provided by withFirebaseAuth HOC */
+  signInWithGoogle,
+  signOut,
+  setError,
+  user,
+  error,
+}: WrappedComponentProps) => (
+  <React.Fragment>
+    {
+      user
+        ? <h1>Hello, {user.displayName}</h1>
+        : <h1>Log in</h1>
+    }
+
+    {
+      user
+        ? <button onClick={signOut}>Sign out</button>
+        : <button onClick={signInWithGoogle}>Sign in with Google</button>
+    }
+  </React.Fragment>
+);
+
+const firebaseAppAuth = firebaseApp.auth();
+
+/** See the signature above to find out the available providers */
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
+
+/** Wrap it */
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(App);
---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyARAROiSfJDBxPPaWcKnPSM6Uly-WAJpt0",
+  authDomain: "react-auth-test-8c9a9.firebaseapp.com",
+  databaseURL: "https://react-auth-test-8c9a9.firebaseio.com",
+  projectId: "react-auth-test-8c9a9",
+  storageBucket: "",
+  messagingSenderId: "960604243787",
+  appId: "1:960604243787:web:53f417204677dad4"
+};
+
+export default firebaseConfig;
